6c7a10d7f7f5cd477d15c3964315ab121c015c82
aoo0703 <119462645+aoo0703@users.noreply.github.com>
2024-05-26 17:48:24 +0900

    add file
    add example file

---
+++ example.py
@@ -0,0 +1,10 @@
+from artcoloranalyzer.analyzer import ArtColorAnalyzer
+
+image_path = "example_image/breakfast.png"
+
+# #  Example in painting - hue change by grid selection
+# image_path = "example_image/00_kaiga.JPG"
+
+analyzer = ArtColorAnalyzer(image_path)
+
+
